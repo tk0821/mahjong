406f3c126a7ea28473a7fd3b1fb15b270bff1007
tk0821 <t_kaneshiro@outlook.com>
2023-11-13 10:48:19 +0900

    add: Initial generation window settings

--- desktop/src/tk0821/mahjong/DesktopLauncher.java
+++ desktop/src/tk0821/mahjong/DesktopLauncher.java
@@ -2,14 +2,16 @@ package tk0821.mahjong;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import tk0821.mahjong.Mahjong;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1280, 720);
 		config.setForegroundFPS(60);
+		config.useVsync(true);
 		config.setTitle("mahjong");
+		config.setResizable(false);
 		new Lwjgl3Application(new Mahjong(), config);
 	}
 }
