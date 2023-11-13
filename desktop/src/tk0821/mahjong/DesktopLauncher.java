801f323a7cbc95cb450713c08448781ca49debbf
tk0821 <t_kaneshiro@outlook.com>
2023-11-13 10:36:47 +0900

    first commit

---
+++ desktop/src/tk0821/mahjong/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package tk0821.mahjong;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import tk0821.mahjong.Mahjong;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("mahjong");
+		new Lwjgl3Application(new Mahjong(), config);
+	}
+}
